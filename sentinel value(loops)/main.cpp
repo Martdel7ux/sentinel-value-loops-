f040fc8389583e81b6988567c76291dd94261981
Martin Ahoto <martinahoto4@gmail.com>
2021-08-30 13:16:40 +0300

    basic

--- sentinel value(loops)/main.cpp
+++ sentinel value(loops)/main.cpp
@@ -7,8 +7,34 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    int total = 0;
+    int grade;
+    double average;
+    int counter = 0;
+    
+    cout<<"enter the grades here -1 to finish"<<endl;
+    cin>>grade;
+    
+    while(grade!=-1)
+    {
+        total=grade+total;
+        counter++;
+        cout<<"enter grade here -1 to finish"<<endl;
+        cin>>grade;
+    }
+    
+    if(counter!=0)
+    {
+        average=total/counter;
+        cout<<"average is"<<average<<endl;
+    }
+    else
+    {
+        cout<<"no grade was entered into the system"<<endl;
+    }
+    
     return 0;
 }
